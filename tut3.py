e43f5732bd6481de8447ca8121a6fe4645444dea
Hrithik Kumar <62447081+hrithik-21@users.noreply.github.com>
2020-07-18 02:19:49 +0530

    Delete tut3.py

--- tut3.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask,render_template
-a=Flask(__name__)
-@a.route('/')
-def hello():
-    name1="Hrithik"
-    return render_template('index.html',name=name1)   #It will return index.html which must be located in templates folder
-a.run(debug=True)
